9c50f7e1da4855b9a4ed3a8f19d498835da7ad96
friedSaewoo <zriag@naver.com>
2025-02-11 19:35:10 +0900

    first commit

---
+++ src/main/java/com/example/jpa_schedule/JpaScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpa_schedule/JpaScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
